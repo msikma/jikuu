f98c1885b3e9835a29e905212bdfd4ac7ce386cb
Michiel Sikma <michiel@sikma.org>
2018-09-20 18:44:08 +0200

    Add system font option

--- src/decorator/gallery.js
+++ src/decorator/gallery.js
@@ -3,13 +3,19 @@
 
 import { Luminous, LuminousGallery } from 'luminous-lightbox'
 
+// Our option modifications.
+const jikuuLumOpts = {
+  // Attach to #root so we can style it based on the user's settings.
+  appendToSelector: '#root'
+}
+
 // Slight modification of the LuminousGallery to allow captions on a per-image basis.
 export default class LuminousGalleryCaptions extends LuminousGallery {
   _constructLuminousInstances() {
     this.luminousInstances = []
     for (let i = 0; i < this.triggers.length; i++) {
       const caption = el => el.getAttribute('data-caption')
-      const lum = new Luminous(this.triggers[i], { ...this.luminousOpts, caption })
+      const lum = new Luminous(this.triggers[i], { ...this.luminousOpts, caption, ...jikuuLumOpts })
       this.luminousInstances.push(lum)
     }
   }
